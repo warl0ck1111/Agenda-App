0519b30c194a461ce9bd5f72fdc33ae249303017
warl0ck1111 <bashirokala@gmail.com>
2021-07-16 19:31:46 +0100

    add entity model

---
+++ src/main/java/com/example/demo/models/Agenda.java
@@ -0,0 +1,67 @@
+package com.example.demo.models;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @author Okala III
+ */
+
+@Data
+@Entity
+public class Agenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "DATE")
+    private LocalDate date;
+
+    @Column(name = "DETAILS")
+    private String details;
+
+
+    @Column(name = "COMPLETED")
+    private Boolean isCompleted = false;
+
+    @Column(name = "TIME")
+    private String time;
+
+    @Column(name = "IS_IMPORTANT")
+    private Boolean isImportant = false;
+
+
+    @Column(name = "IS_DELETED")
+    private boolean isDeleted = false;
+
+    @CreationTimestamp
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd h:m:s")
+    @Column(name = "TIME_CREATED")
+    private LocalDateTime timeCreated;
+
+    @UpdateTimestamp
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd h:m:s")
+    @Column(name = "TIME_UPDATED")
+    private LocalDateTime timeUpdated;
+
+
+
+
+
+
+
+}
