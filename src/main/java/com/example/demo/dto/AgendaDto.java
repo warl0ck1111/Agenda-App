8a3dfdcbbecc634deb6a9e8438489293bb72eead
warl0ck1111 <bashirokala@gmail.com>
2021-07-16 19:35:00 +0100

    add dto files

---
+++ src/main/java/com/example/demo/dto/AgendaDto.java
@@ -0,0 +1,38 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @author Okala III
+ */
+
+@Data
+public class AgendaDto {
+
+    private Long id;
+
+    private String name;
+
+    private String title;
+
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE, pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    private String details;
+
+    private Boolean isCompleted ;
+
+    private String time;
+
+    private Boolean isImportant;
+
+    private Boolean isDeleted = false;
+
+
+}
