ab1e3be82707aef30fd406269e20ae465e4dbdcd
warl0ck1111 <bashirokala@gmail.com>
2021-07-16 19:29:07 +0100

    add controller to expose CRUD service Api

---
+++ src/main/java/com/example/demo/controller/AgendaController.java
@@ -0,0 +1,61 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.AgendaDto;
+import com.example.demo.response.JsonResponse;
+import com.example.demo.services.impl.AgendaService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Okala III
+ */
+
+@Slf4j
+@RestController
+@RequestMapping("api/agenda")
+@CrossOrigin
+
+public class AgendaController {
+
+    @Autowired
+    private AgendaService agendaService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createAgenda(@RequestBody AgendaDto dto){
+        System.out.println(dto);
+        agendaService.create(dto);
+        return new ResponseEntity<>("Agenda created Successfully", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateAgenda(@RequestBody AgendaDto dto){
+        System.out.println(dto);
+        agendaService.update(dto);
+        return new ResponseEntity<>("Item Updated", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteAgenda(@PathVariable(name = "id") Long id){
+        agendaService.delete(id);
+        return new ResponseEntity<>("Agenda Deleted", HttpStatus.OK);
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<?> findAll(){
+        log.info("entered....Get all");
+        return new ResponseEntity<>(agendaService.findAll(), HttpStatus.OK);
+    }
+
+ @GetMapping("/get-all/completed")
+    public ResponseEntity<?> findAllCompleted(){
+        log.info("entered....Get all...completed");
+        return new ResponseEntity<>(agendaService.findAllCompleted(), HttpStatus.OK);
+    }
+
+
+
+}
